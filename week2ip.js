a51d47518843cf35051d17c80345c5629f5f312e
Gideon Mkangaa <gideonm77@outlook.com>
2021-11-15 12:25:04 +0300

    Made changes to the styling in the .css file.Added majority of the js script controling the input and output of the data.

--- week2ip.js
+++ week2ip.js
@@ -6,17 +6,17 @@ function getInput(){
   month = parseInt(document.getElementById("month").value);
   dayOfMonth = parseInt(document.getElementById("monthday").value);
 
-  if(century == ""){
-    alert("Input the correct gender");
+  if(century == "" || century < 0 || century> 2100 ){
+    alert("Please Enter A Valid Century");
     return false;
-  }else if (year == ""){
-    alert("Input the correct year");
+  }else if (year == "" || year > 2021){
+    alert("Please Enter A Valid Year");
     return false;
-  }else if (month == ""){
-    alert("Input the correct month");
+  }else if (month == "" || month <= 0 || month > 12){
+    alert("Please Enter A Valid Month");
     return false;
-  }else if(dayOfMonth == ""){
-    alert("input the correct date");
+  }else if(dayOfMonth == "" || dayOfMonth <=0  || dayOfMonth > 31){
+    alert("Please Enter A Valid Day Of The Month");
     return false;
   }
 }
@@ -35,13 +35,9 @@ function checkDayOfWeek(){
      console.log("The function runs");//Test chackDayOfWeek function
 }
 
-//arrays
-let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
 //get selected radio button
-function checkGender(){
+function checkGender()
+{
     var gen = document.getElementsByName("rads");
     if(gen[0].checked == true){
         var gender = "male";
@@ -82,29 +78,30 @@ function checkGender(){
         case gender = "female":
             switch(day){
               case 0 || -0:
-                document.getElementById("result").innerHTML = "The day was a Sunday." + "  " + "Your akan name is  akosua";
+                document.getElementById("result").innerHTML = "The day was a Sunday." + "  " + "Your akan name is " + femaleNames[0];
               break;
               case 1 || -1:
-                document.getElementById("result").innerHTML = "The day was a Monday." + " " + "Your akan name is adwoa ";
+                document.getElementById("result").innerHTML = "The day was a Monday." + " " + "Your akan name is "  + femaleNames[1];
               break;
               case 2 || -2:
-                document.getElementById("result").innerHTML = "The day was a Tuesday." + " " + "Your akan name is abenaa";
+                document.getElementById("result").innerHTML = "The day was a Tuesday." + " " + "Your akan name is " + femaleNames[2];
               break;
               case 3 || -3:
-                document.getElementById("result").innerHTML = "The day was a Wednesday." + " " + "Your akan name is akua";
+                document.getElementById("result").innerHTML = "The day was a Wednesday." + " " + "Your akan name is " + femaleNames[3];
               break;
               case 4 || -4:
-                document.getElementById("result").innerHTML = "The day was a Thursday." + " " + "Your akan name is yaa";
+                document.getElementById("result").innerHTML = "The day was a Thursday." + " " + "Your akan name is " + femaleNames[4];
               break;
               case 5 || -5:
-                document.getElementById("result").innerHTML = "The day was a Friday." + " " + "Your akan name is afua";
+                document.getElementById("result").innerHTML = "The day was a Friday." + " " + "Your akan name is " + femaleNames[5];
               break;
               case 6 || -6:
-                document.getElementById("result").innerHTML = "The day was a Saturday." + " " + "Your akan name is ama";
+                document.getElementById("result").innerHTML = "The day was a Saturday." + " " + "Your akan name is " + femaleNames[6];
               break;
 
           }
     break
     default:
     console.log("pass");//Test gender switch
-}
+        }
+    }
